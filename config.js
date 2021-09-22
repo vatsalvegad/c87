0ee4559f712158c7d9f62cc8d600ef5bcad70dc3
vatsalvegad <76203776+vatsalvegad@users.noreply.github.com>
2021-09-22 14:55:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdV7jRlFxIjrnkZ_4lDmGYyIjZ-SwkXms",
+  authDomain: "story-telling-app-7f2aa.firebaseapp.com",
+  databaseURL: "https://story-telling-app-7f2aa-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-7f2aa",
+  storageBucket: "story-telling-app-7f2aa.appspot.com",
+  messagingSenderId: "213574183993",
+  appId: "1:213574183993:web:2115cb0f14fd85e414f1e8",
+  measurementId: "G-VZVWRPG50L"
+};
+
+
+  
